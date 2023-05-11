fb86a8d30e8e18fb5dadf1791fc354aadba4bc6e
NatanDuarte <natanprocopio@gmail.com>
2023-05-11 20:42:46 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  io.emit('Welcome to the chat room!');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
